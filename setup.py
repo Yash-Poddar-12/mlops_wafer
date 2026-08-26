50481b1a104ccf77e719a5ed1bba6a4e96951047
Yash-Poddar-12 <yapoddar@gmail.com>
2026-08-26 20:49:38 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It is a wafer project using MLOPs',
+    author='Yash-Poddar-12',
+    license='MIT',
+)
